a85208b4b531fc063a4046b152b9818cc62d881b
davidtang <davidtang1977@outlook.com>
2018-08-11 02:35:16 -0400

    change namespace
    change namesapce to WebMarket

---
+++ WebMarket/WebMarket.Data.Repository/IShoppingCartRepository.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using WebMarket.Data.Model;
+
+namespace WebMarket.Data.Repository
+{
+    public interface IShoppingCartRepository
+    {
+        ShoppingCart GetById(int id);
+        ShoppingCart Create();
+        ShoppingCart Update(int id, ShoppingCart newValue);
+    }
+}
